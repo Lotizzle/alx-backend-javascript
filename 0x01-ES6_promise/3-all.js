2d6bb77846ccb00841e884234ca65fda7b54121a
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-11 02:11:08 +0000

    Resolved all promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResponse, userResponse] = results;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch((error) => {
+      console.error('Signup system offline');
+    });
+}
