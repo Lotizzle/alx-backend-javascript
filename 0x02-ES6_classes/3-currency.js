0dcd3a026ca87406aec7c380413e7c46113c298f
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-12 16:14:37 +0000

    Logging currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and setter for code
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Getter and setter for name
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Method
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
