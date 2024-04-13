d9d09789743f5be053855ee163ac97317a2c614e
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-13 07:33:17 +0000

    Created Pricing class by importing Currency class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+/*eslint-disable*/
+
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and setter for currency
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
