f687ab5b27c618d9bda502308653ba9b1b4773ea
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-26 14:57:19 +0000

    Created Asynchronous function

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,27 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  let userResult; let
+    photoResult;
+
+  try {
+    userResult = await userPromise;
+  } catch (error) {
+    userResult = error;
+  }
+
+  try {
+    photoResult = await photoPromise;
+  } catch (error) {
+    photoResult = error;
+  }
+
+  return [
+    { status: userResult instanceof Error ? 'rejected' : 'fulfilled', value: userResult },
+    { status: photoResult instanceof Error ? 'rejected' : 'fulfilled', value: photoResult },
+  ];
+}
