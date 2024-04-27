c6b846c9260ac27708e830eb35abec0b19299c01
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-27 20:57:27 +0000

    Searching for match

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
-  }
-
   const values = [];
   for (const value of set) {
     if (value.startsWith(startString)) {
