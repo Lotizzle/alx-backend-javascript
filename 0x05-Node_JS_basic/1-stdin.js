b8262cb1f34ee979d54c4324d44e701fa1fbecd9
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-05 01:24:14 +0100

    Accessing database

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
-  process.stdin.end();
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
