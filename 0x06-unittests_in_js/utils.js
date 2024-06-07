78af2890a269c57135d5767ef0c78b7bca201a8a
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-07 17:56:57 +0100

    Spying

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+// utils.js
+const Utils = {
+    calculateNumber: (type, a, b) => {
+        a = Math.round(a);
+        b = Math.round(b);
+
+	if (type === 'SUM') {
+	    return a + b;
+	} else if (type === 'SUBTRACT') {
+	    return a - b;
+	} else if ( type === 'DIVIDE') {
+	    if (b === 0) {
+	        return 'Error';
+	    }
+	    return a / b;
+	} else {
+	    throw new Error('Invalid operation type');
+	}
+    }
+};
+
+module.exports = Utils;
