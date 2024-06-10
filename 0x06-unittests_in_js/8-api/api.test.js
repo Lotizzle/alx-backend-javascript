25bed7c0379ec5a495e0b6acb259f1aa6df39d54
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-10 20:42:53 +0100

    New api and testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,28 @@
+// api.test.js
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./api'); // Assuming your server is exported from api.js
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('Index Page', () => {
+  it('should return status code 200', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should return the correct result', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  });
+});
