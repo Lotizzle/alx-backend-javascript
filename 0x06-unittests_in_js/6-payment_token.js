46c050d352453ff82f8457aa47d5df0d61e2f9da
Lotizzle <132350248+Lotizzle@users.noreply.github.com>
2024-07-12 00:15:49 +0100

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-// 6-payment_token.js
-
-function getPaymentTokenFromAPI(success) {
-    if (success) {
-        return Promise.resolve({ data: 'Successful response from the API' })
-    }
-}
-
-module.exports = getPaymentTokenFromAPI;
