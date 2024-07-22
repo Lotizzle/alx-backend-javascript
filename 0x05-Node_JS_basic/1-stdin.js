b7c26898494b7e350ddf52bcec82c26aaf0e5247
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-07-22 17:43:51 +0100

    Testing process

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// 1-stdin.js
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
+
+process.on('SIGINT', () => {
+  process.stdout.write('\nThis important software is now closing\n');
+  process.exit();
+});
